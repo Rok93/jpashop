0341fcd2a38fd8cac1cf97f67bd0fcdd5ad4f9a6
rok93 <goodboy302@naver.com>
2021-02-14 20:43:58 +0900

    take class: 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -13,4 +13,13 @@ public class Address { // Address는 JPA의 내장타입이기 때문에 Embedda
     private String street;
 
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }

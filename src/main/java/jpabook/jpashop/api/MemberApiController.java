98bc27e9e2123f8f7a3d88d9f1b2c18cc28f8300
rok93 <goodboy302@naver.com>
2021-04-08 13:04:03 +0900

    task class: 회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,12 +9,41 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -54,9 +83,15 @@ public class MemberApiController {
     }
 
     @Data
+    static class MemberResponse {
+
+    }
+
+    @Data
     @AllArgsConstructor
     static class UpdateMemberResponse {
         private Long id;
+
         private String name;
 
     }
@@ -65,5 +100,6 @@ public class MemberApiController {
     static class UpdateMemberRequest {
         @NotEmpty
         private String name;
+
     }
 }

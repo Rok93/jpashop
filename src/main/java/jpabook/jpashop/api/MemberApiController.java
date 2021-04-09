d2ee97078c58387bea9f14653efd559d8b0f200e
rok93 <goodboy302@naver.com>
2021-04-09 10:25:58 +0900

    task class: 조회용 샘플 데이터 입력

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -24,13 +24,13 @@ public class MemberApiController {
     }
 
     @GetMapping("/api/v2/members")
-    public Result membersV2() {
+    public Result<List<MemberDto>> membersV2() {
         List<Member> findMembers = memberService.findMembers();
         List<MemberDto> collect = findMembers.stream()
                 .map(m -> new MemberDto(m.getName()))
                 .collect(Collectors.toList());
 
-        return new Result(collect);
+        return new Result<>(collect);
     }
 
     @Data
